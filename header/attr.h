bdbef5112fb10788a4eb40c95c97448b1f3528a2
fadhil riyanto <me@fadev.org>
2024-12-23 20:13:16 +0700

    add GCC atrribute

---
+++ header/attr.h
@@ -0,0 +1,9 @@
+#ifndef ATTR_H
+#define ATTR_H
+
+#define __always_inline                 inline __attribute__((__always_inline__))
+#define __cold                          __attribute__((__cold__))
+#define __hot                          __attribute__((__hot__))
+
+
+#endif
